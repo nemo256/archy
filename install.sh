f5819e33cbf45b69aac3f2a6c7ab3e613a351772
Amine Neggazi <neggazimedlamine@gmail.com>
2023-01-05 00:21:23 +0100

    added install and postinstall scripts

---
+++ install.sh
@@ -0,0 +1,374 @@
+#!/usr/bin/env bash
+
+# Set a bold custom terminus font
+setfont ter-v32b
+
+################################################
+#                                              #
+# Create a .env file containing these options: #
+#                                              #
+################################################
+#
+# USERNAME=<New user name>
+# PASSWORD=<New user password>
+# HOSTNAME=<Hostname eg: macbook>
+# SHELL=<eg: /bin/bash>
+# TOKEN=<Your Github token (PAT)>
+# DISK=<eg: /dev/sda>
+# MOUNT_OPTIONS=<eg for ssd add: "noatime,ssd,...">
+# FS=<Filesystem eg: ext4, btrfs...>
+# TIMEZONE=<eg: Europe/Paris>
+# KEYMAP=<eg: us,uk,es...>
+
+source /root/.env
+
+# Logo
+clear
+echo -ne "
+------------------------------------------------------------------------------------------
+                                Automated Archy Installer
+------------------------------------------------------------------------------------------
+"
+
+echo -ne "
+                               Press any key to continue...
+"
+read
+
+echo -ne "
+------------------------------------------------------------------------------------------
+                                   Formating Disks
+------------------------------------------------------------------------------------------
+"
+umount -A --recursive /mnt # make sure everything is unmounted before we start
+# Disk prep
+sgdisk -Z ${DISK} # zap all on disk
+sgdisk -a 2048 -o ${DISK} # new gpt disk 2048 alignment
+
+# Create partitions
+sgdisk -n 1::+128M --typecode=1:af00 --change-name=1:'Boot Loader' ${DISK} # partition 1 (Boot loader Partition)
+sgdisk -n 2::+256M --typecode=2:8300 --change-name=2:'Boot' ${DISK} # partition 2 (Boot Partition)
+sgdisk -n 3::-0 --typecode=3:8300 --change-name=3:'Root' ${DISK} # partition 3 (Root), default start, remaining space
+
+if [[ ! -d "/sys/firmware/efi" ]]; then # checking for bios system
+    sgdisk -A 1:set:2 ${DISK}
+fi
+partprobe ${DISK} # reread partition table to ensure it is correct
+
+# Make filesystems
+echo -ne "
+------------------------------------------------------------------------------------------
+                                  Creating Filesystems
+------------------------------------------------------------------------------------------
+"
+partition2=${DISK}2
+partition3=${DISK}3
+
+mkfs.fat -F32 ${partition2}
+mkfs.ext4 -L Root ${partition3}
+mount -t ext4 ${partition3} /mnt
+
+# Mount target
+mkdir /mnt/boot
+mount ${partition2} /mnt/boot/
+
+if ! grep -qs '/mnt' /proc/mounts; then
+    echo "Drive is not mounted, can not continue"
+    echo "Rebooting in 3 Seconds ..." && sleep 1
+    echo "Rebooting in 2 Seconds ..." && sleep 1
+    echo "Rebooting in 1 Second ..." && sleep 1
+    reboot now
+fi
+
+echo -ne "
+------------------------------------------------------------------------------------------
+                                    Archy installation
+------------------------------------------------------------------------------------------
+"
+
+# This is a better approache because it doesnt have to update everytime!
+echo "Pacstraping... "
+pacstrap /mnt \
+  alsa-utils \
+  arch-install-scripts \
+  archlinux-keyring \
+  archinstall \
+  archiso \
+  b43-fwcutter \
+  base \
+  bind-tools \
+  brltty \
+  broadcom-wl-dkms \
+  btrfs-progs \
+  clonezilla \
+  cloud-init \
+  crda \
+  cryptsetup \
+  darkhttpd \
+  ddrescue \
+  dhclient \
+  diffutils \
+  dmraid \
+  dnsmasq \
+  dosfstools \
+  e2fsprogs \
+  edk2-shell \
+  efibootmgr \
+  espeakup \
+  ethtool \
+  exfatprogs \
+  f2fs-tools \
+  fatresize \
+  fsarchiver \
+  fzf \
+  gnu-netcat \
+  gpart \
+  gpm \
+  gptfdisk \
+  grml-zsh-config \
+  grub \
+  hdparm \
+  intel-ucode \
+  ipw2100-fw \
+  ipw2200-fw \
+  irssi \
+  iw \
+  iwd \
+  jfsutils \
+  kitty-terminfo \
+  less \
+  lftp \
+  libfido2 \
+  libusb-compat \
+  linux \
+  linux-atm \
+  linux-firmware \
+  linux-firmware-marvell \
+  livecd-sounds \
+  lsscsi \
+  lvm2 \
+  lynx \
+  man-db \
+  man-pages \
+  mc \
+  mdadm \
+  memtest86+ \
+  mkinitcpio \
+  mkinitcpio-archiso \
+  mkinitcpio-nfs-utils \
+  modemmanager \
+  mtools \
+  nano \
+  nbd \
+  ndisc6 \
+  networkmanager \
+  nfs-utils \
+  nilfs-utils \
+  nmap \
+  ntfs-3g \
+  nvme-cli \
+  openconnect \
+  openssh \
+  openvpn \
+  partclone \
+  parted \
+  partimage \
+  pcsclite \
+  ppp \
+  pptpclient \
+  pv \
+  qemu-guest-agent \
+  refind \
+  reflector \
+  reiserfsprogs \
+  rp-pppoe \
+  rsync \
+  rxvt-unicode-terminfo \
+  screen \
+  sdparm \
+  sg3_utils \
+  smartmontools \
+  sof-firmware \
+  squashfs-tools \
+  sudo \
+  syslinux \
+  systemd-resolvconf \
+  tcpdump \
+  terminus-font \
+  testdisk \
+  tmux \
+  tpm2-tss \
+  udftools \
+  usb_modeswitch \
+  usbmuxd \
+  usbutils \
+  neovim \
+  virtualbox-guest-utils-nox \
+  vpnc \
+  wireless-regdb \
+  wireless_tools \
+  wpa_supplicant \
+  wvdial \
+  xfsprogs \
+  xl2tpd \
+  zsh \
+  alsa-firmware \
+  asoundconf \
+  aspell \
+  autoconf \
+  automake \
+  cmake \
+  binutils \
+  bison \
+  bluez \
+  bluez-utils \
+  brightnessctl \
+  dkms \
+  dmidecode \
+  docker \
+  dsniff \
+  dunst \
+  edk2-ovmf \
+  exa \
+  fakeroot \
+  file \
+  findutils \
+  firefox \
+  flex \
+  gawk \
+  gcc \
+  gcr \
+  gettext \
+  git \
+  glib2 \
+  glibc \
+  go \
+  grep \
+  groff \
+  gtk2 \
+  gzip \
+  hping \
+  hspell \
+  htop \
+  hunspell \
+  libcap \
+  libcap-ng \
+  libconfig \
+  libev \
+  libnotify \
+  libpcap \
+  libtool \
+  libvoikko \
+  libx11 \
+  libxinerama \
+  libxft \
+  linux-headers \
+  lsof \
+  m4 \
+  make \
+  mdp \
+  meson \
+  mpc \
+  mpd \
+  mpv \
+  multipath-tools \
+  ncdu \
+  ncmpcpp \
+  neofetch \
+  net-tools \
+  newsboat \
+  nodejs \
+  noto-fonts \
+  noto-fonts-cjk \
+  noto-fonts-extra \
+  npm \
+  nuspell \
+  os-prober \
+  pacman \
+  pacman-contrib \
+  patch \
+  pax-utils \
+  picom \
+  pkgconf \
+  pulsemixer \
+  python-pip \
+  ranger \
+  scrot \
+  sed \
+  stow \
+  sxiv \
+  the_silver_searcher \
+  transmission-cli \
+  tree \
+  ttf-fira-code \
+  ttf-nerd-fonts-symbols \
+  unrar \
+  unzip \
+  upower \
+  uthash \
+  vi \
+  webkit2gtk \
+  which \
+  xclip \
+  xmlstarlet \
+  xorg \
+  xorg-xinit \
+  yarn \
+  yt-dlp \
+  zathura \
+  zathura-pdf-mupdf \
+  zip \
+  libva-intel-driver \
+  libvdpau-va-gl \
+  libva-intel-driver \
+  libva-utils \
+  vulkan-intel \
+  xf86-video-intel
+
+genfstab -L /mnt >> /mnt/etc/fstab
+echo " 
+  Generated /etc/fstab:
+"
+cat /mnt/etc/fstab
+
+echo -ne "
+------------------------------------------------------------------------------------------
+                          GRUB BIOS Bootloader Install And Check
+------------------------------------------------------------------------------------------
+"
+if [[ ${DISK} != /dev/vda ]]; then
+  if [[ ! -d "/sys/firmware/efi" ]]; then
+      grub-install --boot-directory=/mnt/boot ${DISK}
+      echo "Installed bootloader for BIOS!"
+  else
+      echo "Skipping (UEFI)!"
+  fi
+fi
+echo -ne "
+------------------------------------------------------------------------------------------
+                           Checking For Low Memory Systems <8Gb
+------------------------------------------------------------------------------------------
+"
+TOTAL_MEM=$(cat /proc/meminfo | grep -i 'memtotal' | grep -o '[[:digit:]]*')
+if [[  $TOTAL_MEM -lt 8000000 ]]; then
+    # Put swap into the actual system, not into RAM disk, otherwise there is no point in it, it'll cache RAM into RAM. So, /mnt/ everything.
+    mkdir -p /mnt/opt/swap # make a dir that we can apply NOCOW to to make it btrfs-friendly.
+    chattr +C /mnt/opt/swap # apply NOCOW, btrfs needs that.
+    dd if=/dev/zero of=/mnt/opt/swap/swapfile bs=1M count=2048 status=progress
+    chmod 600 /mnt/opt/swap/swapfile # set permissions.
+    chown root /mnt/opt/swap/swapfile
+    mkswap /mnt/opt/swap/swapfile
+    swapon /mnt/opt/swap/swapfile
+    # The line below is written to /mnt/ but doesn't contain /mnt/, since it's just / for the system itself.
+    echo "/opt/swap/swapfile	none	swap	sw	0	0" >> /mnt/etc/fstab # add swap to fstab, so it KEEPS working after installation.
+fi
+
+echo -ne "
+------------------------------------------------------------------------------------------
+                           Chrooting Into The New Installation
+------------------------------------------------------------------------------------------
+"
+# Wifi configuration
+mkdir -p /mnt/var/lib/iwd
+( arch-chroot /mnt /root/postinstall.sh ) |& tee postinstall.log
+cp postinstall.log /mnt/root
---
+++ postinstall.sh
@@ -0,0 +1,347 @@
+#!/usr/bin/env bash
+
+################################################
+#                                              #
+# Create a .env file containing these options: #
+#                                              #
+################################################
+#
+# USERNAME=<New user name>
+# PASSWORD=<New user password>
+# HOSTNAME=<Hostname eg: macbook>
+# SHELL=<eg: /bin/bash>
+# TOKEN=<Your Github token (PAT)>
+# DISK=<eg: /dev/sda>
+# MOUNT_OPTIONS=<eg for ssd add: "noatime,ssd,...">
+# FS=<Filesystem eg: ext4, btrfs...>
+# TIMEZONE=<eg: Europe/Paris>
+# KEYMAP=<eg: us,uk,es...>
+
+source /root/.env
+
+# Logo
+clear
+echo -ne "
+------------------------------------------------------------------------------------------
+                                Archy Post Installation
+------------------------------------------------------------------------------------------
+
+"
+
+echo -ne "
+------------------------------------------------------------------------------------------
+                                     Network Setup
+------------------------------------------------------------------------------------------
+"
+# cp /etc/pacman.d/mirrorlist /etc/pacman.d/mirrorlist.backup
+# reflector -a 48 -c France,Germany -f 5 -l 20 --sort rate --save /etc/pacman.d/mirrorlist
+# mkdir /mnt &>/dev/null # hiding error message if any
+
+echo -ne "
+------------------------------------------------------------------------------------------
+                                Setup Language And Locale
+------------------------------------------------------------------------------------------
+"
+sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
+locale-gen
+timedatectl --no-ask-password set-timezone ${TIMEZONE}
+timedatectl --no-ask-password set-ntp 1
+localectl --no-ask-password set-locale LANG="en_US.UTF-8" LC_TIME="en_US.UTF-8"
+ln -s /usr/share/zoneinfo/${TIMEZONE} /etc/localtime
+# Set keymaps
+localectl --no-ask-password set-keymap ${KEYMAP}
+
+# Add sudo no password rights
+# sed -i 's/^# %wheel ALL=(ALL) NOPASSWD: ALL/%wheel ALL=(ALL) NOPASSWD: ALL/' /etc/sudoers
+# sed -i 's/^# %wheel ALL=(ALL:ALL) NOPASSWD: ALL/%wheel ALL=(ALL:ALL) NOPASSWD: ALL/' /etc/sudoers
+
+# Add parallel downloading
+sed -i 's/^#ParallelDownloads/ParallelDownloads/' /etc/pacman.conf
+
+# Enable multilib
+sed -i "/\[multilib\]/,/Include/"'s/^#//' /etc/pacman.conf
+
+echo -ne "
+------------------------------------------------------------------------------------------
+                                     Adding User
+------------------------------------------------------------------------------------------
+"
+# useradd -m -G wheel,libvirt -s $SHELL $USERNAME 
+# echo "$USERNAME created, home directory created, added to wheel and libvirt group, default shell set to /bin/bash"
+
+pwconv
+echo "${USERNAME}:${PASSWORD}" | chpasswd
+echo "${HOSTNAME}" > /etc/hostname
+
+echo -ne "
+------------------------------------------------------------------------------------------
+                                 Creating GRUB Boot Menu
+------------------------------------------------------------------------------------------
+"
+# Installing GRUB
+if [[ -d "/sys/firmware/efi" ]]; then
+    grub-install --efi-directory=/boot ${DISK} --recheck --force
+fi
+
+# Optimize grub for macbook air and skip through it (I don't multiboot)
+sed -i 's/GRUB_CMDLINE_LINUX_DEFAULT="[^"]*/& rootflags=data=writeback libata.force=1:noncq/' /etc/default/grub
+sed -i 's/^GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/' /etc/default/grub
+sed -i 's/^#GRUB_DISABLE_SUBMENU=y/GRUB_DISABLE_SUBMENU=y/' /etc/default/grub
+
+# Updating grub
+echo -e "Updating grub..."
+grub-mkconfig -o /boot/grub/grub.cfg
+echo -e "All set!"
+
+echo -ne "
+------------------------------------------------------------------------------------------
+                               Managing Essential Services
+------------------------------------------------------------------------------------------
+"
+systemctl disable --now transmission.service
+echo "--> Transmission disabled"
+systemctl disable --now dhcpcd.service
+echo "  DHCP disabled"
+systemctl enable --now NetworkManager.service
+echo "--> NetworkManager enabled"
+
+echo -ne "
+------------------------------------------------------------------------------------------
+                              Changing Default Console Font
+------------------------------------------------------------------------------------------
+"
+echo -ne 'KEYMAP="us"
+FONT="ter-v32b"
+' > /etc/vconsole.conf
+
+echo -ne "
+------------------------------------------------------------------------------------------
+                                   Github Configuration      
+------------------------------------------------------------------------------------------
+"
+# Adding the credentials file
+echo -ne "https://nemo256:${TOKEN}@github.com" > ${HOME}/.git-credentials
+
+echo -ne "
+------------------------------------------------------------------------------------------
+                                Installing Dwm, St, Dmenu...
+------------------------------------------------------------------------------------------
+"
+
+git clone https://github.com/nemo256/dwm
+git clone https://github.com/nemo256/st
+git clone https://github.com/nemo256/dmenu
+git clone https://github.com/nemo256/slock
+git clone https://github.com/nemo256/slstatus
+git clone https://github.com/nemo256/.dotfiles
+
+# Change directory to where the build files are
+cd /root/.build
+# Building and installing using make
+cd dwm && make clean install
+cd ../st && make clean install
+cd ../dmenu && make clean install
+cd ../slock && make clean install
+cd ../slstatus && make clean install
+cd ../ani-cli && chmod +x ani-cli && cp ani-cli /usr/local/bin/ani-cli
+cd ../tty-clock && make && make install
+cd ../grabc && make && make install
+cd ../tremc && make install
+yarn global add @aweary/alder
+yarn global add weather-cli
+
+echo -ne "
+------------------------------------------------------------------------------------------
+                                 Restoring Home Directory
+------------------------------------------------------------------------------------------
+"
+cd $HOME && mkdir Downloads Videos Music Work
+cd $HOME
+
+echo -ne "
+------------------------------------------------------------------------------------------
+                                Stowing Configuration Files
+------------------------------------------------------------------------------------------
+"
+# Removing default files
+rm -fvr ${HOME}/.bash*
+rm -fvr ${HOME}/.gitconfig
+rm -fvr ${HOME}/.config/*
+
+# Dotfiles directory
+cd $HOME/.dotfiles
+
+# Stowing configuration files
+stow abook
+stow alsa
+stow bin
+stow bash
+stow dunst
+stow git
+stow gtk-2.0
+stow gtk-3.0
+stow htop
+stow irssi
+stow mbsync
+stow mimeapps
+stow mpd
+stow mpv
+stow mutt
+stow ncmpcpp
+stow neofetch
+stow newsboat
+stow notmuch
+stow nvim
+stow picom
+stow ranger
+stow sxiv
+stow transmission-daemon
+stow tremc
+stow weather-cli-nodejs
+stow xinit
+stow yarn
+stow zathura
+
+# Making bin files executable
+chmod -R 755 $HOME/bin
+
+echo -ne "
+------------------------------------------------------------------------------------------
+                                   Neovim Configuration
+------------------------------------------------------------------------------------------
+"
+# Adding vim-plug
+# curl -fLo ${HOME}/.vim/autoload/plug.vim --create-dirs \
+    # https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+# Installing neovim plugins
+# nvim -c 'PlugInstall | q! | q!'
+
+# chmod +x $HOME/.config/nvim/autoload/plugged/fzf/install
+
+echo -ne "
+------------------------------------------------------------------------------------------
+                                   Ranger Configuration
+------------------------------------------------------------------------------------------
+"
+# Adding devicons to ranger
+git clone https://github.com/alexanderjeurissen/ranger_devicons \
+  $HOME/.config/ranger/plugins/ranger_devicons
+
+cd $HOME/.config/ranger/plugins/ranger_devicons/
+python -m devicons
+cd $HOME
+
+echo -ne "
+------------------------------------------------------------------------------------------
+                                   Firefox Configuration
+------------------------------------------------------------------------------------------
+"
+# To generate config folder
+#firefox
+
+# Saving path to prefs.js file
+#prefs=$(find $HOME/.mozilla/ -name '*prefs.js')
+
+# Adding magnet link support
+#echo -ne '
+#user_pref("network.protocol-handler.expose.magnet", false);
+#' >> $prefs
+
+echo -ne "
+------------------------------------------------------------------------------------------
+                                    Fonts Configuration
+------------------------------------------------------------------------------------------
+"
+# Adding nerd font (Droid Sans Mono)
+# mkdir -p ${HOME}/.fonts
+# cd ${HOME}/.fonts && curl -fLo "Fira Code Bold Nerd Font Complete.ttf" https://github.com/ryanoasis/nerd-fonts/blob/master/patched-fonts/FiraCode/Bold/complete/Fira%20Code%20Bold%20Nerd%20Font%20Complete.ttf
+
+# Update fonts
+# fc-cache -f -v
+
+# Return back to home directory
+# cd $HOME
+
+echo -ne "
+------------------------------------------------------------------------------------------
+                                   Disabling Screen Tear
+------------------------------------------------------------------------------------------
+"
+# Adding TearFree option
+echo -ne 'Section "Device"
+  Identifier "Intel Graphics"
+  Driver "intel"
+  Option "TearFree" "true"
+EndSection
+' > /etc/X11/xorg.conf.d/20-intel.conf
+
+echo -ne "
+------------------------------------------------------------------------------------------
+                                    Slock Configuration
+------------------------------------------------------------------------------------------
+"
+# Adding slock service
+echo -ne '[Unit]
+Description=Lock X session using slock for user %i
+Before=sleep.target
+
+[Service]
+User=%i
+Environment=DISPLAY=:0
+ExecStartPre=/usr/bin/xset dpms force suspend
+ExecStart=/usr/local/bin/slock
+
+[Install]
+WantedBy=sleep.target
+' > /etc/systemd/system/slock@.service
+
+echo -ne "
+------------------------------------------------------------------------------------------
+                               Updating Touchpad Configuration
+------------------------------------------------------------------------------------------
+"
+# Touchpad configuration
+echo -ne 'Section "InputClass"
+    Identifier "libinput touchpad catchall"
+    MatchIsTouchpad "on"
+    MatchDevicePath "/dev/input/event*"
+    Option "Tapping" "True"
+    Option "TappingDrag" "True"
+    Option "ScrollMethod" "Twofinger"
+    Option "NaturalScrolling" "False"
+    Option "DisableWhileTyping" "False"
+    Driver "libinput"
+EndSection
+' > /etc/X11/xorg.conf.d/40-libinput.conf
+
+echo -ne "
+------------------------------------------------------------------------------------------
+                               Re-enabling Essential Services
+------------------------------------------------------------------------------------------
+"
+# Enabling slock to lock screen on suspend / sleep
+systemctl enable slock@$(whoami).service
+echo "  Slock enabled!"
+
+echo -ne "
+------------------------------------------------------------------------------------------
+                                         Cleaning
+------------------------------------------------------------------------------------------
+"
+# Remove no password sudo rights
+sed -i 's/^%wheel ALL=(ALL) NOPASSWD: ALL/# %wheel ALL=(ALL) NOPASSWD: ALL/' /etc/sudoers
+sed -i 's/^%wheel ALL=(ALL:ALL) NOPASSWD: ALL/# %wheel ALL=(ALL:ALL) NOPASSWD: ALL/' /etc/sudoers
+# Add sudo rights
+sed -i 's/^# %wheel ALL=(ALL) ALL/%wheel ALL=(ALL) ALL/' /etc/sudoers
+sed -i 's/^# %wheel ALL=(ALL:ALL) ALL/%wheel ALL=(ALL:ALL) ALL/' /etc/sudoers
+
+rm -fvr $HOME/.postinstall.sh $HOME/.env
+
+echo -ne "
+------------------------------------------------------------------------------------------
+                               Automated Archy Installer
+------------------------------------------------------------------------------------------
+                            Done - Please Eject Install Media
+
+"
